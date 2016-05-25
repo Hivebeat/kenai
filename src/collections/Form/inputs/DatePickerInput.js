0a867200e87ea80753ae6befbd028844ac6a180f
hagbarth <emil@hivebeat.com>
2016-05-25 22:30:21 +0200

    change datepicker props

--- src/collections/Form/inputs/DatePickerInput.js
+++ src/collections/Form/inputs/DatePickerInput.js
@@ -1,18 +1,16 @@
 import React, { PropTypes } from 'react'
 import InputWrapper from './InputWrapper'
 
-const DatePickerInput = ({ onChange, openModal, title, id, value}) => (
+const DatePickerInput = ({ openDatePicker, title value}) => (
   <div className="ui action input">
-    <input onChange={onChange} ref="value" type="text" placeholder={title} id={id} value={value} readOnly style={{ cursor: 'not-allowed' }} />
-    <button onClick={openModal} className="ui huge icon button"><i className="calendar icon"></i></button>
+    <input ref="value" type="text" placeholder={title} value={value} readOnly style={{ cursor: 'not-allowed' }} />
+    <button onClick={openDatePicker} className="ui huge icon button"><i className="calendar icon"></i></button>
   </div>
 )
 
 DatePickerInput.propTypes = {
-  onChange: PropTypes.func,
-  openModal: PropTypes.func,
+  openDatePicker: PropTypes.func,
   title: PropTypes.string,
-  id: PropTypes.string,
   value: PropTypes.string
 }
 
